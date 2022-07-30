60d507aed7fa15f22af8421a3eb3087cac9f81a8
Jan-Eric Bednarek <debednaj@krohne.com>
2022-07-30 12:28:46 +0200

    Idk Structure

--- FileManager/obj/Debug/net6.0-windows10.0.22000.0/Views/Container.g.i.cs
+++ FileManager/obj/Debug/net6.0-windows10.0.22000.0/Views/Container.g.i.cs
@@ -99,7 +99,7 @@ namespace FileManager.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPF UI - Demo App;V1.0.0.0;component/views/container.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPF UI - Demo App;component/views/container.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\Container.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
