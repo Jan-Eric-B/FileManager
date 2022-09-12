0732cff429c94c3f4b4658122cacef3619a956cb
Jan-Eric Bednarek <debednaj@krohne.com>
2022-09-12 11:51:02 +0200

    Added function to select Text from List of items

---
+++ FileManager/Services/SelectableTextBlock.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FileManager.Services
+{
+    /// <summary>
+    /// SelectableTextBlock
+    /// Custom TextBLock for selecting Text in List
+    /// https://stackoverflow.com/a/45627524/12273101
+    /// </summary>
+    public class SelectableTextBlock : TextBlock
+    {
+        private readonly TextEditorWrapper _editor;
+
+        static SelectableTextBlock()
+        {
+            FocusableProperty.OverrideMetadata(typeof(SelectableTextBlock), new FrameworkPropertyMetadata(true));
+            TextEditorWrapper.RegisterCommandHandlers(typeof(SelectableTextBlock), true, true, true);
+
+            // remove the focus rectangle around the control
+            FocusVisualStyleProperty.OverrideMetadata(typeof(SelectableTextBlock), new FrameworkPropertyMetadata((object)null));
+        }
+        public SelectableTextBlock()
+        {
+            _editor = TextEditorWrapper.CreateFor(this);
+        }
+    }
+}
