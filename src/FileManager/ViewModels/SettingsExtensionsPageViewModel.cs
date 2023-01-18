b77e0aa41069fdd9659c8e4a9dc574a9995453be
Jan-Eric Bednarek <debednaj@krohne.com>
2023-01-18 10:57:52 +0100

    Making Extensions installable. Working on Settings Window

---
+++ src/FileManager/ViewModels/SettingsExtensionsPageViewModel.cs
@@ -0,0 +1,61 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using FileManager.Models;
+using FileManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Windows.UI.Composition.Scenes;
+using Wpf.Ui.Common.Interfaces;
+using Wpf.Ui.Mvvm.Contracts;
+using Wpf.Ui.Mvvm.Services;
+
+namespace FileManager.ViewModels
+{
+    public class SettingsExtensionsPageViewModel : ObservableObject, INavigationAware
+    {
+      
+
+        private readonly ISnackbarService _snackbarService;
+
+        #region Main
+
+        private bool _dataInitialized;
+
+        public ContainerViewModel Container { get; set; }
+
+        #endregion
+
+        //____________________________________________________________
+
+        #region Main
+
+        public SettingsExtensionsPageViewModel(ContainerViewModel container, ISnackbarService snackbarService)
+        {
+            Container = container;
+            _snackbarService = snackbarService;
+        }
+
+        public void OnNavigatedFrom()
+        {
+        }
+
+        public void OnNavigatedTo()
+        {
+            if (!_dataInitialized)
+            {
+                InitializeData();
+            }
+        }
+
+        private void InitializeData()
+        {
+            _dataInitialized = true;
+        }
+
+        #endregion
+
+
+
+
+    }
+}
--- src/FileManager/Views/Pages/SettingsExtensionsPage.xaml.cs
+++ src/FileManager/Views/Pages/SettingsExtensionsPage.xaml.cs
@@ -1,13 +1,63 @@
-﻿namespace FileManager.Views.Pages
+﻿using FileManager.Services;
+using FileManager.ViewModels;
+using System.Diagnostics;
+using System.Drawing;
+using Wpf.Ui.Mvvm.Interfaces;
+using System.Windows.Media;
+
+namespace FileManager.Views.Pages
 {
     /// <summary>
     /// Interaction logic for SettingsExtensionsPage.xaml
     /// </summary>
     public partial class SettingsExtensionsPage
     {
+        bool ExifToolInstalled = false;
+
         public SettingsExtensionsPage()
         {
+            DataContext = ViewModel;
+
             InitializeComponent();
+
+
+            if (ExtensionsService.CheckExistance("exiftool(-k).exe"))
+            {
+                btExifToolInstalled.Icon = Wpf.Ui.Common.SymbolRegular.CheckmarkCircle24;
+                btExifToolInstalled.Foreground = System.Windows.Media.Brushes.Green;
+            }
+            else
+            {
+                btExifToolInstalled.Icon = Wpf.Ui.Common.SymbolRegular.DismissCircle24;
+                btExifToolInstalled.Foreground = System.Windows.Media.Brushes.Red;
+
+
+            }
+
+        }
+
+        public SettingsExtensionsPageViewModel ViewModel
+        {
+            get;
+        }
+
+        private void btExifToolDownload_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+            ExtensionsService.DownloadApplication("exiftool(-k).exe");
+
+        }
+        
+        private void btFfmpegDownload_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+            ExtensionsService.DownloadApplication("exiftool(-k).exe");
+
+        }
+
+        private void btOpenExtensionDirectory_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
         }
     }
 }
