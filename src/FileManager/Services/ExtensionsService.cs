0d0c6a9b8c28cb39590b9b690f012a218d986047
Jan-Eric Bednarek <debednaj@krohne.com>
2023-01-18 10:58:32 +0100

    Added Extension Service Class

---
+++ src/FileManager/Services/ExtensionsService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileManager.Models;
+using Windows.Media.Protection.PlayReady;
+using System.Net;
+
+namespace FileManager.Services
+{
+    public static class ExtensionsService
+    {
+        public static bool CheckExistance(string file)
+        {
+            string applicationPath = System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+
+
+            return EditFileSevice.Check(applicationPath + "\\" + file);
+        }
+
+        public static string GetPath(string file)
+        {
+            string applicationPath = System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+
+            return applicationPath + "\\Extensions\\" + file;
+        }
+
+
+        public static void DownloadApplication(string file)
+        {
+            if (file == "exiftool(-k).exe")
+            {
+                StartProcessService.Start("https://exiftool.org/", false, false);
+            }
+            else if (file == "ffmpeg.exe")
+            {
+                StartProcessService.Start("https://www.gyan.dev/ffmpeg/builds/", false, false);
+            }
+        }
+
+
+
+}
+}
